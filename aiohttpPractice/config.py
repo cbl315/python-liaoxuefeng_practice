4653dfc253d69ac67904b4e6325dcdaeb612e86c
hfutcbl <583605554@qq.com>
2016-11-06 13:30:48 +0800

    Delete config.py

--- aiohttpPractice/config.py
+++
@@ -1,9 +0,0 @@
-# config.py
-import config_dafault
-configs = config_dafault.configs
-
-try:
-    import config_override
-    configs = merge(configs, config_override.configs)
-except ImportError:
-    pass
